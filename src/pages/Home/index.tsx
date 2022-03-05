feafa4ce20c94ce69f7009ec6146fd69b3f9c0fb
Nicolas Oliveira <nicksouza861@gmail.com>
2022-03-05 18:28:20 -0300

    feat: added banner slider

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,65 @@
 import React from "react";
+import Flicking, { ViewportSlot } from "@egjs/react-flicking";
+import "@egjs/react-flicking/dist/flicking.css";
+import { Arrow, Pagination } from "@egjs/flicking-plugins";
+import Banner from "assets/img/banner-home.png";
+import BannerMobile from "assets/img/banner-home-mobile.png";
+import ArrowIcon from "assets/svg/arrow.svg";
 import { Header } from "components/Header";
-import { Container } from "./styles";
+import { Container, Panel } from "./styles";
 
 const Home: React.FC = () => {
+  const plugins = [new Arrow(), new Pagination()];
+
   return (
     <Container>
       <Header />
+      <Flicking align="prev" circular plugins={plugins}>
+        <Panel>
+          <img
+            src={window.outerWidth > 768 ? Banner : BannerMobile}
+            alt="Banner"
+          />
+
+          <div className="content">
+            <span className="title">Promoções de Outono</span>
+            <p className="description">
+              Confiras os melhores looks para combinar com você nesse Outono
+            </p>
+            <button type="button" className="btn">
+              Conferir
+            </button>
+          </div>
+        </Panel>
+        <Panel>
+          <img
+            src={window.outerWidth > 768 ? Banner : BannerMobile}
+            alt="Banner"
+          />
+          <div className="content">
+            <span className="title">Promoções de Outono</span>
+            <p className="description">
+              Confiras os melhores looks para combinar com você nesse Outono
+            </p>
+            <button type="button" className="btn">
+              Conferir
+            </button>
+          </div>
+        </Panel>
+
+        <ViewportSlot>
+          <span className="flicking-arrow-prev">
+            <img src={ArrowIcon} alt="Arrow Icon" />
+          </span>
+          <span className="flicking-arrow-next">
+            <img src={ArrowIcon} alt="Arrow Icon" />
+          </span>
+        </ViewportSlot>
+
+        <ViewportSlot>
+          <div className="flicking-pagination" />
+        </ViewportSlot>
+      </Flicking>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,4 +3,136 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: relative;
   min-height: 100vh;
+
+  .flicking-viewport {
+    .flicking-arrow-prev,
+    .flicking-arrow-next {
+      position: absolute;
+      z-index: 10;
+      top: 50%;
+      cursor: pointer;
+    }
+
+    .flicking-arrow-prev {
+      left: 161px;
+      transform: translateY(-50%);
+    }
+
+    .flicking-arrow-next {
+      right: 161px;
+      transform: translateY(-50%) rotate(180deg);
+    }
+
+    @media screen and (max-width: 900px) {
+      .flicking-arrow-prev {
+        left: 28px;
+      }
+
+      .flicking-arrow-next {
+        right: 28px;
+      }
+    }
+
+    .flicking-pagination {
+      display: flex;
+      position: absolute;
+      bottom: 16px;
+      left: 50%;
+      transform: translateX(-50%);
+      z-index: 11;
+
+      .flicking-pagination-bullet {
+        border: 1px solid #faa500;
+        border-radius: 50%;
+        height: 8px;
+        width: 8px;
+        display: block;
+        margin-right: 16px;
+
+        :last-child {
+          margin-right: 0;
+        }
+
+        &-active {
+          background: #faa500;
+        }
+      }
+    }
+  }
+`;
+
+export const Panel = styled.div`
+  width: 100%;
+  position: relative;
+
+  > img {
+    max-width: 100%;
+    object-fit: contain;
+
+    @media screen and (max-width: 900px) {
+      object-fit: cover;
+      min-width: 100%;
+      max-width: unset;
+    }
+  }
+
+  .content {
+    position: absolute;
+    top: 151px;
+    left: 323px;
+
+    @media screen and (max-width: 900px) {
+      left: 63px;
+      top: 63px;
+    }
+
+    .title {
+      font-family: Montserrat, sans-serif;
+      font-weight: 700;
+      font-size: 2.5rem;
+      line-height: 49px;
+      color: #ffffff;
+
+      @media screen and (max-width: 900px) {
+        font-size: 1.875rem;
+        line-height: 36px;
+      }
+    }
+
+    .description {
+      font-family: Montserrat, sans-serif;
+      font-weight: 400;
+      font-size: 1.25rem;
+      line-height: 24px;
+      color: #ffffff;
+      margin: 40px 0 32px;
+      width: 467px;
+
+      @media screen and (max-width: 900px) {
+        font-size: 0.875rem;
+        line-height: 17px;
+        width: 70%;
+        margin: 24px 0 16px;
+      }
+    }
+
+    .btn {
+      width: 128px;
+      background: #faa500;
+      border-radius: 10px;
+      font-size: 1rem;
+      line-height: 20px;
+      color: #ffffff;
+      padding: 14px 0;
+      font-weight: 700;
+
+      @media screen and (max-width: 900px) {
+        width: 76px;
+        height: 28px;
+        padding: 0;
+        border-radius: 4px;
+        font-size: 0.875rem;
+      }
+    }
+  }
 `;
